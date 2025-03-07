fb178a4ffcce705407c3b213bc029ac89e8a09e2
Dominik Opitz <dominik.opitz@dlr.de>
2025-03-07 09:09:17 +0100

    Updated Zwischenstand

--- opitz_models/agents/ValidatorAgent.py
+++ opitz_models/agents/ValidatorAgent.py
@@ -3,19 +3,14 @@ from agents.Agent import Agent
 from utils import extract_json_from_string
 
 SYSTEM_PROMPT = """
-You are part of a larger system that generates critical questions to to interventions of political debates.
-Other agents of this system have already created possible critical questions for the intervention.
-Your task in this system is to assess the quality of the generated critical questions with respect to the original intervention. Avoid evaluating the intervention itself, this is not the task.
-
-The user will provide you with both the original intervention and a corresponding critical question.
-Assess the validity and usefulness of the question with regards to the intervention based on your role: {role}.
+You are a professor for critical reasoning and logic.
+Given a critical question along with a corresponding argument, your task is to judge the critical question for its validity. As part of a larger validator system, you have the following sub-role in this task: {subrole}
 """
 
 EVALUATION_PROMPT = """
-The intervention is this:
-
+The argument is this:
 ```
-{intervention}
+{argument}
 ```
 
 The corresponding critical question for you to evaluate is this:
@@ -23,15 +18,20 @@ The corresponding critical question for you to evaluate is this:
 {critical_question}
 ```
 
-Your role is: {role}.
+Your subrole for this task is: {subrole}.
+End your response with a brief sentence about the questions validity.
 """
 
 ASSESSMENT_PROMPT = """
-Based on your reasoning, do you assess the critical question to be useful?
-Strictly return your answer in the following JSON format:  
+Return your final answer as a JSON.
+If you assess the critical question to be valid, return True.
+If you assess the critical question to be invalid, return False.
+
+Here is the JSON structure for you to return.
+
 ```json
 {{
-  'is_useful': <True/False>,
+  'is_valid': <True/False>,
 }}
 ```
 """
@@ -40,24 +40,27 @@ Strictly return your answer in the following JSON format:
 class ValidatorAgent(Agent):
     def __init__(
         self,
-        intervention: str,
-        role: str,
+        argument: str,
+        subrole: str,
         model_name: str,
         model_parameters: dict = {},
     ):
         super().__init__(model_name, model_parameters)
 
         # Agent specific parameters
-        self.intervention = intervention
-        self.role = role
+        self.argument = argument
+        self.subrole = subrole
         self.model_name = model_name
 
     def _initialize(self):
         # Initialize system prompt
-        message = SYSTEM_PROMPT.format(role=self.role)
+        message = SYSTEM_PROMPT.format(subrole=self.subrole)
         self._add_to_chat(role="system", message=message)
 
-    def evaluate_critical_question(self, critical_question: str) -> dict[str, str]:
+    def _clear_chat(self):
+        self.chat = []
+
+    def evaluate_critical_question(self, critical_question: str) -> Optional[dict[str, str]]:
         """Evaluates the provided critical question and assesses whether it is qualitative enough.
 
         Args:
@@ -66,29 +69,38 @@ class ValidatorAgent(Agent):
         Returns:
             dict[str, str]: The evaluation result. Dictionary that contains the key 'is_useful' and 'feedback'.
         """
-        # Step 1: Evaluation: LLM evaluates quality of the question
-        evaluation_prompt = EVALUATION_PROMPT.format(
-            intervention=self.intervention, critical_question=critical_question, role=self.role
-        )
-        self._add_to_chat(role="user", message=evaluation_prompt)
-        feedback = self.single_response(messages=self.chat)
-        self._add_to_chat(role="assistant", message=feedback)
+        is_response_valid = False
+        retry = 0
+        max_retries = 3
+        while not is_response_valid:
+            # Step 1: Evaluation: LLM evaluates quality of the question
+            evaluation_prompt = EVALUATION_PROMPT.format(
+                argument=self.argument, critical_question=critical_question, subrole=self.subrole
+            )
+            self._add_to_chat(role="user", message=evaluation_prompt)
+            feedback = self.single_response(messages=self.chat)
+            self._add_to_chat(role="assistant", message=feedback)
 
-        # Step 2: Assessment: LLM assesses whether question is "good enough"
-        assessment_prompt = ASSESSMENT_PROMPT
-        self._add_to_chat(role="user", message=assessment_prompt)
-        response = self.single_response(messages=self.chat)
-        self._add_to_chat(role="assistant", message=response)
+            # Step 2: Assessment: LLM assesses whether question is "good enough"
+            assessment_prompt = ASSESSMENT_PROMPT
+            self._add_to_chat(role="user", message=assessment_prompt)
+            response = self.single_response(messages=self.chat)
+            self._add_to_chat(role="assistant", message=response)
 
-        # Process response
-        response_dict: dict = extract_json_from_string(response)
-        is_useful = response_dict.get("is_useful", None)
-        if not is_useful:
-            print("Error: LLM returned an invalid response. Expected JSON with key 'is_useful', but no JSON with that key was found.")
-            # TODO Implement some retries here.
+            # Process response
+            response_dict: dict = extract_json_from_string(response)
+            is_valid = response_dict.get("is_valid", None)
+            if is_valid == None:
+                retry += 1
+                if retry >= max_retries:
+                    evaluation = None
+                    print("LLM did not output a valid response. Discarding result.")
+                    break
+                self._clear_chat()
+                self._initialize()
+                continue
+            is_response_valid = True
 
-        evaluation = {"is_useful": is_useful, "feedback": feedback}
-
-        # TODO: If not 'useful', then prompt again to summarize the feedback.
+            evaluation = {"is_valid": is_valid, "feedback": feedback}
 
         return evaluation
--- opitz_models/main.py
+++ opitz_models/main.py
@@ -2,7 +2,7 @@ from agents.GeneratorAgent import GeneratorAgent
 from agents.ValidatorAgent import ValidatorAgent
 from utils import print_text_bold, print_text_cyan, print_text_orange
 
-intervention = """
+argument = """
 CLINTON: "The central question in this election is really what kind of country we want to be and what kind of future we 'll build together
 Today is my granddaughter 's second birthday
 I think about this a lot
@@ -18,47 +18,77 @@ I also want to see more companies do profit-sharing"
 """
 
 ### Generator Agent ###
-model_name = "llama3.2:3b-instruct-q5_K_M"  # llama3.2:1b, llama3:latest
-generator = GeneratorAgent(intervention, model_name)
+model_name = "llama3.2:latest"  # llama3.2:1b, llama3:latest
+generator = GeneratorAgent(argument, model_name)
 
 ### Validator Agents ###
-model_name = "llama3.2:3b-instruct-q5_K_M"  # llama3.2:1b, llama3:latest
-role = "Your role is to assess whether any invalid relations between claims and the premises provided to support those claims are drawn. Specifically check whether the question uncovers the blind spots in argumentation"
-validator_1 = ValidatorAgent(intervention, role, model_name)
-validators = [validator_1]
+# Validator 1
+model_name = "llama3.2:latest"  # llama3.2:1b, llama3:latest
+subrole = "Specifically pay attention to whether the critical question introduces any new concepts or topics, that were not explicitly mentioned in the argument. An introduction of an unmentioned topic or concept would render the question invalid."
+validator_1 = ValidatorAgent(argument, subrole, model_name)
 
-### Generation Loop ###
-print_text_bold(intervention)
+# Validator 2
+model_name = "llama3.2:latest"  # llama3.2:1b, llama3:latest
+subrole = "Specifically assess whether any invalid relations between claims and the premises provided to support those claims are drawn. Specifically check whether the question uncovers any blind spots in argumentation. If invalid relations between claims and premises are drawn, this would render the question invalid."
+validator_2 = ValidatorAgent(argument, subrole, model_name)
 
+# Validator 3
+model_name = "llama3.2:latest"  # llama3.2:1b, llama3:latest
+subrole = "Specifically evaluate whether the critical question contains bad reasoning, namely, questions critical positions or claims that the speaker does not hold. If that is the case, it would render the question invalid."
+validator_3 = ValidatorAgent(argument, subrole, model_name)
+
+# Validator 4
+model_name = "llama3.2:latest"  # llama3.2:1b, llama3:latest
+subrole = "Specifically assess whether the critical question is non-specific, meaning that it could be asked on any argument and is not focused on the argument at hand. If the question is too unspecific, it would render it invalid."
+validator_4 = ValidatorAgent(argument, subrole, model_name)
+
+validators = [validator_1, validator_2, validator_3, validator_4]
+
+
+### Generation ###
+print_text_bold(argument)
 question_valid = False
 feedback = []
-while not question_valid:
-    # Generate initial question
-    critical_question = generator.generate_critical_question(feedback)
-    print_text_cyan("Generator Agent:")
-    print_text_cyan(critical_question)
 
-    # Evaluate question
-    evaluations = []
-    for validator in validators:
-        evaluation = validator.evaluate_critical_question(critical_question)
+# Generate initial question
+critical_question = generator.generate_critical_question()
+print_text_cyan("Generator Agent:")
+print_text_cyan(critical_question)
+
+# Evaluate question
+evaluations = []
+for validator in validators:
+    evaluation = validator.evaluate_critical_question(critical_question)
+    if evaluation == None:
+        continue
+    else:
         evaluations.append(evaluation)
         print()
-        print_text_orange(f"Validator Agent: Critical Question is useful: {evaluation['is_useful']}")
+        print("################################################")
+        print()
+        print_text_orange(
+            f"Validator Agent: Critical Question is useful: {evaluation['is_valid']}"
+        )
         print_text_orange(evaluation["feedback"])
 
-    # Check if all evaluations are positive
-    if all(evaluation["is_useful"] for evaluation in evaluations):
-        question_valid = True
-    else:
-        # Gather and concatenate feedbacks where 'is_useful' is False
-        feedbacks = [
-            f"### Feedback:\n{evaluation['feedback']}\n\n\n"
-            for evaluation in evaluations
-            if not evaluation["is_useful"]
-        ]
+# Check if all evaluations are positive
+if all(evaluation["is_valid"] for evaluation in evaluations):
+    question_valid = True
+else:
+    # Gather and concatenate feedbacks where 'is_valid' is False
+    feedbacks = [
+        f"### Feedback:\n{evaluation['feedback']}\n\n\n"
+        for evaluation in evaluations
+        if not evaluation["is_valid"]
+    ]
 
-        # Concatenate the feedbacks into a single string
-        feedback = "".join(feedbacks)
+    # Concatenate the feedbacks into a single string
+    feedback = "".join(feedbacks)
+
+print()
+print()
+print("###### Final Feedback:")
+print()
+print(feedbacks)
 
 print("\n\nDone.")
